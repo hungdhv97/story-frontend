b72e86bf2a3da87d31bf8dc263f7fefb4a53a3d4
hungdhv97 <hungdhv97@gmail.com>
2024-01-04 13:35:15 +0700

    Sort import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import React from 'react';
 
 import './globals.css';
-import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
