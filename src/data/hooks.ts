2e4cbc9d0d7b0e1221c4c884fdf6ec98bcd5f4b8
hungdhv97 <hungdhv97@gmail.com>
2024-01-11 15:34:46 +0700

    Hooks: add useGetGotStories hook

--- src/data/hooks.ts
+++ src/data/hooks.ts
@@ -5,3 +5,7 @@ import { getData } from '@/data/server';
 export const useGetGenres = () => {
     return useSWR('http://18.141.25.103:8000/api/genres/', getData);
 };
+
+export const useGetHotStories = () => {
+    return useSWR('http://18.141.25.103:8000/api/stories/?is_hot=true&limit=10', getData);
+};
