d939912e884ec959f3ae88b2c714f40aa44a9fe6
hungdhv97 <hungdhv97@gmail.com>
2024-01-12 10:38:42 +0700

    H1 component: rename H1Props to IH1Props

--- src/components/common/custom.tsx
+++ src/components/common/custom.tsx
@@ -2,14 +2,17 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
-interface H1Props {
+interface IH1Props {
     href: string;
     children: ReactNode;
     className?: string;
 }
 
-const H1: FC<H1Props> = ({ href, children, className }) => (
-    <Link href={href} passHref>
+const H1: FC<IH1Props> = ({ href, children, className }) => (
+    <Link
+        href={href}
+        passHref
+    >
         <h1
             className={clsx(
                 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-2 hover:underline',
