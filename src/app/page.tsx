5321ec04e5caa12191b10954a5caee9f761a2692
hungdhv97 <hungdhv97@gmail.com>
2024-01-12 16:04:24 +0700

    HomePage: Add useAtom setMethod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import { useAtom } from 'jotai';
 import React from 'react';
 
 import Footer from '@/components/home/footer';
 import Header from '@/components/home/header';
 import MainContent from '@/components/home/main';
 
+import { fullStoriesResponseAtom } from '@/atoms/fullStoriesResponseAtom';
+import { genresResponseAtom } from '@/atoms/genresResponseAtom';
+import { hotStoriesResponseAtom } from '@/atoms/hotStoriesResponseAtom';
+import { updatedStoriesResponseAtom } from '@/atoms/updatedStoriesResponseAtom';
+
 export default function HomePage() {
+    const [, setGenresResponse] = useAtom(genresResponseAtom);
+    const [, setHotStoriesResponse] = useAtom(hotStoriesResponseAtom);
+    const [, setFullStoriesResponse] = useAtom(fullStoriesResponseAtom);
+    const [, setUpdatedStoriesResponse] = useAtom(updatedStoriesResponseAtom);
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
