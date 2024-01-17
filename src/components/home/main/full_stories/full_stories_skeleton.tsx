de8a32786bfaf7047b680a0456946b9939e8c7c9
hungdhv97 <hungdhv97@gmail.com>
2024-01-17 09:16:56 +0700

    FullStoriesSkeleton: Change to 8

--- src/components/home/main/full_stories/full_stories_skeleton.tsx
+++ src/components/home/main/full_stories/full_stories_skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 export function FullStoriesSkeleton() {
     return (
         <div className="grid gap-8 grid-cols-4">
-            {Array.from({ length: 10 }).map((_, index) => (
+            {Array.from({ length: 8 }).map((_, index) => (
                 <Skeleton key={index} className="min-h-[350px] rounded-2xl" />
             ))}
         </div>
