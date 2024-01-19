7513a62c82f9dbdec04bedda53997a3453ae4c5f
hungdhv97 <hungdhv97@gmail.com>
2024-01-19 18:04:42 +0700

    ChapterPage: Create Breadcrumb component

--- src/app/stories/[storySlug]/chapters/[chapterId]/page.tsx
+++ src/app/stories/[storySlug]/chapters/[chapterId]/page.tsx
@@ -4,14 +4,17 @@ import { useEffect } from 'react';
 
 import { saveChapterHistory } from '@/lib/storage';
 
-import { useGetChapterById } from '@/data/hooks';
+import { useGetChapterById, useGetStoryByStorySlug } from '@/data/hooks';
+
+import { Breadcrumb } from '@/components/Common/Breadcrumb/Breadcrumb';
 
 export default function ChapterPage({
     params,
 }: {
-    params: { chapterId: string };
+    params: { chapterId: string; storySlug: string };
 }) {
-    const { data: chapter, isLoading } = useGetChapterById(params.chapterId);
+    const { data: chapter } = useGetChapterById(params.chapterId);
+    const { data: story } = useGetStoryByStorySlug(params.storySlug);
 
     useEffect(() => {
         if (chapter) {
@@ -19,6 +22,20 @@ export default function ChapterPage({
         }
     }, [chapter]);
 
-    if (isLoading) return <div>Loading...</div>;
-    return <div>Detail Chapter Page {chapter?.title}</div>;
+    if (!chapter) return <div>Chapter loading...</div>;
+    if (!story) return <div>Story loading...</div>;
+
+    const paths = [
+        { title: 'Home', href: '/' },
+        { title: `${story.title}`, href: `/stories/${story.slug}` },
+        {
+            title: `${chapter.title}`,
+            href: `/stories/${story.slug}/chapters/${chapter.id}`,
+        },
+    ];
+    return (
+        <div className="container">
+            <Breadcrumb paths={paths} />
+        </div>
+    );
 }
