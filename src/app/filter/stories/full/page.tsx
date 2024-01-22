6abb37cf16fd51883730d97b6c12c20cb258213c
hungdhv97 <hungdhv97@gmail.com>
2024-01-22 22:46:52 +0700

    FullStoryListPage: Add Breadcrumb component

--- src/app/filter/stories/full/page.tsx
+++ src/app/filter/stories/full/page.tsx
@@ -1,8 +1,15 @@
+import { Breadcrumb } from '@/components/Common/Breadcrumb/Breadcrumb';
 import { FilterTable } from '@/components/FilterPage/FilterTable/FilterTable';
 
 export default function FullStoryListPage() {
+    const paths = [
+        { title: 'Home', href: '/' },
+        { title: 'Truyện Full', href: '/filter/stories/full' },
+    ];
+
     return (
-        <div>
+        <div className="container">
+            <Breadcrumb paths={paths} />
             <FilterTable />
         </div>
     );
