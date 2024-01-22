be74b7b585dddcd9e376a8e86f27dba0a6741251
hungdhv97 <hungdhv97@gmail.com>
2024-01-22 14:05:24 +0700

    UpdatedStoryTable: Change filter genres href

--- src/components/HomePage/UpdatedStoryList/UpdatedStoryTable/UpdatedStoryTable.tsx
+++ src/components/HomePage/UpdatedStoryList/UpdatedStoryTable/UpdatedStoryTable.tsx
@@ -69,7 +69,7 @@ export function UpdatedStoryTable() {
                                         {index > 0 && ', '}
                                         <Link
                                             className="text-blue-600 hover:text-blue-800 visited:text-purple-600"
-                                            href={`/genres/${genre.slug}`}
+                                            href={`/filter/genres/${genre.slug}`}
                                             passHref
                                         >
                                             {genre.name}
