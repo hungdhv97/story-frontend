38c0c2377c66376df312f74ba505e998ee052d5a
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2024-02-22 22:06:17 +0700

    SearchInput: Fix css

--- src/components/Layout/Header/SearchInput/SearchInput.tsx
+++ src/components/Layout/Header/SearchInput/SearchInput.tsx
@@ -25,22 +25,28 @@ export function SearchInput() {
                 className="w-full border pl-4"
                 placeholder="Tìm truyện..."
             />
-            <div className="absolute z-50 mt-2 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto">
-                <div className="origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover p-2 text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]">
-                    {searchStoryList?.map((story) => (
-                        <Link
-                            key={story.id}
-                            href={`/stories/${story.slug}`}
-                            className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                        >
-                            <div className="text-green-500">{story.title}</div>
-                            <div className="text-red-500">
-                                {story.author.name}
-                            </div>
-                        </Link>
-                    ))}
+            {searchStoryList && (
+                <div className="absolute z-50 mt-1.5 w-full rounded-md border bg-popover p-2 text-popover-foreground shadow-lg ">
+                    {searchStoryList.length > 0 ? (
+                        searchStoryList.map((story) => (
+                            <Link
+                                key={story.id}
+                                href={`/stories/${story.slug}`}
+                                className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                            >
+                                <div className="text-green-500">
+                                    {story.title}
+                                </div>
+                                <div className="text-red-500">
+                                    {story.author.name}
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <div className="p-3">Không tìm thấy kết quả ...</div>
+                    )}
                 </div>
-            </div>
+            )}
         </div>
     );
 }
